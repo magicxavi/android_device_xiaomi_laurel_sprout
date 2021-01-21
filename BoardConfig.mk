914980ab422fbdb80a55c307adfb4259880e9a33
Magicxavi <rolfpeterkesche@gmail.com>
2021-01-21 04:51:26 +0200

    laurel_sprout: move to aosp clang
    Also add some Kbuild configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,10 +49,10 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
+export KBUILD_BUILD_USER=Magicxavi
+export KBUILD_BUILD_HOST=MotherRussia
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
