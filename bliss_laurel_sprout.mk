14b0decbeb3a48b8ebd1b6cc2ee245dd0f038c0a
Magicxavi <rolfpeterkesche@gmail.com>
2021-05-06 18:48:40 +0300

    laurel_sprout: update redfing fingerprint to May

--- bliss_laurel_sprout.mk
+++ bliss_laurel_sprout.mk
@@ -38,7 +38,7 @@ TARGET_VENDOR_PRODUCT_NAME := laurel_sprout
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="laurel_sprout-user 11 RKQ1.200903.002 V12.0.7.0.RFQMIXM release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
 
 PRODUCT_PACKAGES += \
     RemovePackages
