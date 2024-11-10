8124a4d37ab4e67221ab7ba54b292352bb978129
magicxavi <rolfpeterkesche@gmail.com>
2024-11-10 19:18:39 +0200

    laurel_sprout: Allow gettid call for atfwd
    We never know what upstream is thinking:
    W ATFWD-daemon: libminijail[4756]: failed to get path of fd 3: No such file or directory
    W ATFWD-daemon: libminijail[4756]: allowing syscall: connect
    W ATFWD-daemon: libminijail[4756]: allowing syscall: fcntl
    W ATFWD-daemon: libminijail[4756]: allowing syscall: sendto
    W ATFWD-daemon: libminijail[4756]: allowing syscall: socket
    W ATFWD-daemon: libminijail[4756]: allowing syscall: writev
    W ATFWD-daemon: libminijail[4756]: logging seccomp filter failures
    E ATFWD-daemon: libminijail[4756]: blocked syscall: gettid

--- extract-files.sh
+++ extract-files.sh
@@ -42,6 +42,10 @@ function blob_fixup() {
         vendor/lib64/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v34.so" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            echo 'gettid: 1' >> ${2}
+            ;; 
     esac
 }
 
